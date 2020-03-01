ec86ca6dddbe767ca0f12b8c25ad34c69b043e0c
nickshevr <nickshevr@yandex-team.ru>
2020-03-01 18:13:16 +0300

    Simple implementation

---
+++ index.js
@@ -0,0 +1,30 @@
+const ora = require('ora');
+
+module.exports = class SimpleProgressPlugin {
+    constructor(options) {
+        this.options = options || {};
+        this.name = 'SimpleProgressPlugin';
+    }
+    apply(compiler) {
+        const {buildName} = this.options;
+
+        if (!buildName) {
+            return;
+        }
+
+        const spinner = ora(`Running ${buildName} build`)
+
+        compiler.hooks.beforeRun.tap(this.name, () => {
+            spinner.start();
+        });
+
+        compiler.hooks.done.tap(this.name, () => {
+            spinner.succeed();
+        });
+
+        compiler.hooks.failed.tap(this.name, () => {
+            spinner.stop();
+        });
+    }
+};
+
